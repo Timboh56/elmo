c922ef9d1cbb0faf431f0abdb4a351f22f37b285
Damien <damienbrockmann@gmail.com>
2013-02-17 14:17:45 +0100

    added sms_codes controller
    Conflicts:
    	.DS_Store

---
+++ app/controllers/sms_codes_controller.rb
@@ -0,0 +1,23 @@
+class SmsCodesController < ApplicationController
+  # render :text => n, :status => 200
+  def show
+  	@form_id= params[:form_id]
+    make_code
+    render :template => 'sms_codes/show.txt.erb'
+  end
+ 
+ 
+  def make_code()	
+		form = Form.find(@form_id)		
+		@codeText = []
+		
+		SmsCode.delete_all(:form_id => @form_id)				
+		qing = form.questionings.select { |q| q.question.type.name == 'select_one' || q.question.type.name == 'select_multiple' || q.question.type.name == 'integer'}	
+		qing.each_with_index do |q, n|
+			nn = n + 1
+  			code_text = q.add_sms_code(nn)
+  			@codeText << nn.to_s + '. ' + q.question.name + "\n" + code_text			
+		end
+	end
+
+end
