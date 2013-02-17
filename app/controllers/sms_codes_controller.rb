2537468f3d0cca12d002444dd8811f893da2aa54
Damien <damienbrockmann@gmail.com>
2013-02-17 21:07:19 +0100

    Active Record error handler added to sms_codes controller

--- app/controllers/sms_codes_controller.rb
+++ app/controllers/sms_codes_controller.rb
@@ -8,15 +8,20 @@ class SmsCodesController < ApplicationController
  
  
   def make_code()	
-		form = Form.find(@form_id)		
 		@codeText = []
 		
-		SmsCode.delete_all(:form_id => @form_id)				
-		qing = form.questionings.select { |q| q.question.type.name == 'select_one' || q.question.type.name == 'select_multiple' || q.question.type.name == 'integer'}	
-		qing.each_with_index do |q, n|
-			nn = n + 1
-  			code_text = q.add_sms_code(nn)
-  			@codeText << nn.to_s + '. ' + q.question.name + "\n" + code_text			
+		begin	
+			@form = Form.find(@form_id)			
+		rescue ActiveRecord::RecordNotFound
+			@codeText << 'Form Not Found!'
+		else			
+			SmsCode.delete_all(:form_id => @form_id)				
+			qing = form.questionings.select { |q| q.question.type.name == 'select_one' || q.question.type.name == 'select_multiple' || q.question.type.name == 'integer'}	
+			qing.each_with_index do |q, n|
+				nn = n + 1
+				code_text = q.add_sms_code(nn)
+				@codeText << nn.to_s + '. ' + q.question.name + "\n" + code_text			
+			end
 		end
 	end
 
