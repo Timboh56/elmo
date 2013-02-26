a933800196848ded14b430005a8e339d14538d83
Damien <damienbrockmann@gmail.com>
2013-02-26 18:03:59 +0100

    reversed prev changes - issue with editing options of published forms

--- app/controllers/sms_codes_controller.rb
+++ app/controllers/sms_codes_controller.rb
@@ -10,33 +10,20 @@ class SmsCodesController < ApplicationController
   end
  
   def make_code()	
-		alpha_index = ('a'..'z').to_a	
-		@sms_qings = []
+		codeText = []
 		
 		@form = Form.find(@form_id)
-		unless @form == nil			
+		@sms_qings = []
+		unless @form == nil
+			SmsCode.delete_all(:form_id => @form_id)				
+			
 			# only prints qings that are not hidden, not conditional, and are of the type (select_one, select_multiple or integer)
 			qings = @form.questionings.select { |q| q.hidden == false && q.condition == nil && (q.question.type.name == 'select_one' || q.question.type.name == 'select_multiple' || q.question.type.name == 'integer')}	   
 			
 			qings.each_with_index do |qing, n|
-				@code_text = []
-				qn = n + 1
-				
-				# save sms_question_number
-				qing.question.update_attributes(:sms_question_no => qn )
-				qing.question.save
-				
-				# save sms_code to options
-				type = qing.question.type.name
-				if type == 'select_one' || type == 'select_multiple'
-					qing.question.option_set.sorted_options.each_with_index do |option, nn|
-						option.update_attributes(:sms_code => alpha_index.fetch(n))
-						option.save	
-						@code_text << alpha_index.fetch(nn)+ '. '+option.name	
-					end
-				end
-				
-				sms_qing = {:qing => qing, :code_text => @code_text}				
+				nn = n + 1
+				code_text = SmsCode.load_sms_code_and_get_text(qing, nn)	
+				sms_qing = {:qing => qing, :code_text => code_text}				
 				@sms_qings << sms_qing
 			end
 		end
